2373b0d3f6e38e3959a7719200da42636a1ccfb2
MrBin99 <MrBin99@users.noreply.github.com>
2024-01-08 19:48:31 +0100

    feat(git): install and configure git

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ set -e
 
 export ANSIBLE_ROLES_PATH=$PWD/roles
 
-ansible-playbook -K -M ./library/* -i inventory.ini playbooks/install.yml
+ansible-playbook -K -M ./library/* -i inventory.ini playbooks/install.yml -e '{"cwd": "'$PWD'"}'
