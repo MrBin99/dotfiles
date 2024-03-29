c45677a732aeb70215589b363557389c7c98728e
MrBin99 <MrBin99@users.noreply.github.com>
2024-03-29 21:01:05 +0100

    feat(nvim): add configurations and plugins for telescope

--- modules/nvim/.config/nvim/lua/plugins/telescope.lua
+++ modules/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,27 +6,49 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
     branch = "0.1.x",
     dependencies = {
+      "nvim-telescope/telescope-ui-select.nvim",
       "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("telescope").setup({
+      local telescope = require("telescope")
+      local telescope_themes = require("telescope.themes")
+
+      -- Configure Telescope.
+      telescope.setup({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules/.*",
+            ".git/.*",
+          },
+        },
         pickers = {
           find_files = {
             hidden = true,
           },
         },
+        extenstions = {
+          ["ui-select"] = {
+            telescope_themes.get_dropdown(),
+          },
+        },
       })
+
+      -- Enable Telescope extensions.
+      telescope.load_extension("ui-select")
 
       -- Keybindings.
       local telescope_builtin = require("telescope.builtin")
       local keybindings = require("helpers.keybindings")
 
-      keybindings.map("n", "<leader>ff", telescope_builtin.find_files, "Files")
-      keybindings.map("n", "<leader>fg", telescope_builtin.live_grep, "Grep")
-      keybindings.map("n", "<leader>fb", telescope_builtin.buffers, "Buffer")
-      keybindings.map("n", "<leader>fh", telescope_builtin.help_tags, "Help")
+      keybindings.map("n", "<leader>ff", telescope_builtin.find_files, { desc = "[F]ind [F]ile" })
+      keybindings.map("n", "<leader>fg", telescope_builtin.live_grep, { desc = "[F]ind [G]rep" })
+      keybindings.map("n", "<leader>fe", telescope_builtin.diagnostics, { desc = "[F]ind [E]rrors" })
+      keybindings.map("n", "<leader>fb", telescope_builtin.buffers, { desc = "[F]ind [B]uffer" })
+      keybindings.map("n", "<leader>fh", telescope_builtin.help_tags, { desc = "[F]ind [H]elp" })
     end,
   },
 }
