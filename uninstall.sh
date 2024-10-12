71cf6eb30fe5f0bd2f00921f28cd748470064595
MrBin99 <MrBin99@users.noreply.github.com>
2024-10-12 22:12:15 +0200

    feat: allow to choose if you want to install all modules or some
    This can be useful when you want to update a single module or test a new
    thing.

--- uninstall.sh
+++ uninstall.sh
@@ -4,4 +4,10 @@ set -e
 
 export ANSIBLE_ROLES_PATH=$PWD/roles
 
-ansible-playbook -K -M ./library/* -i inventory.ini playbooks/uninstall.yml -e '{"cwd": "'$PWD'"}'
+if [[ $# > 0 ]]; then
+  ANSIBLE_ARGS="cwd=$PWD roles_to_run='$@'"
+else
+  ANSIBLE_ARGS="cwd=$PWD roles_to_run=all"
+fi
+
+ansible-playbook -K -M ./library/* -i inventory.ini playbooks/uninstall.yml -e $ANSIBLE_ARGS
