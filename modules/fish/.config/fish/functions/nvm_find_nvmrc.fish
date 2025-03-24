e12ab96573d857270ec7cc086db141ff65da9889
MrBin99 <MrBin99@users.noreply.github.com>
2025-03-24 21:24:56 +0100

    feat(fish): replace oh my fish with fisher
    Oh My Fish seems to not be maintained anymore.

--- modules/fish/.config/fish/functions/nvm_find_nvmrc.fish
+++ modules/fish/.config/fish/functions/nvm_find_nvmrc.fish
@@ -1,3 +1,5 @@
 function nvm_find_nvmrc
-  bass source $NVM_ROOT/nvm.sh --no-use ';' nvm_find_nvmrc
+  if type bass &> /dev/null
+    bass source $NVM_ROOT/nvm.sh --no-use ';' nvm_find_nvmrc
+  end
 end
