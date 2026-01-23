785469d88b9b2f3e20acc2b9fafd45d844c12ce3
MrBin99 <MrBin99@users.noreply.github.com>
2026-01-23 20:41:18 +0100

    feat(lazydocker): install lazydocker

--- modules/fish/.config/fish/config.fish
+++ modules/fish/.config/fish/config.fish
@@ -52,6 +52,9 @@ alias df "duf"
 # Alias "fd"
 alias fd "fdfind"
 
+# Alias "lazydocker"
+alias lzd "lazydocker"
+
 # Git
 alias lg "lazygit"
 alias glo "git log --graph --pretty --format='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an> %C(cyan)<%ae>%Creset' --abbrev-commit"
