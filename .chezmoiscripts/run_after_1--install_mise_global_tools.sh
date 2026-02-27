4ae7c5be8111903211b0b85f45c4706e687095b6
MrBin99 <MrBin99@users.noreply.github.com>
2026-02-27 22:42:37 +0100

    fix(mise): make sure location of mise binary is in the path

--- .chezmoiscripts/run_after_1--install_mise_global_tools.sh
+++ .chezmoiscripts/run_after_1--install_mise_global_tools.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Make sure that the local bin is in the PATH,
+# so that we can use the tools installed by mise.
+PATH="$HOME/.local/bin:$PATH"
+
 # Add a local mise config that is not managed by chezmoi.
 # This allows to install tools specific to this machine.
 if [[ ! -f "$HOME/.config/mise/config.local.toml" ]]; then
